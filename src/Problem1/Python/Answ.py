6daffbda61fe61560f14e40205880ba10cfb797c
Tyler T. Nguyen <tyler.t.nguyen@uconn.edu>
2019-10-21 15:56:31 -0400

    Updated data structure type to make it one-pass

--- src/Problem1/Python/Answ.py
+++ src/Problem1/Python/Answ.py
@@ -1,13 +1,17 @@
-from itertools import permutations
-
 class Answ():
     def __init__ (self, array, targ):
         self.array = array
         self.targ = targ
     def checkArray(self):
-        sol = [pair for pair in permutations(self.array, 2) if sum(pair) == self.targ]
-        return sol != None
+        seen = {}
+        for i in self.array:
+            diff = self.targ - i
+            if i in seen:
+                return (i, seen[i])
+            seen[diff] = i
 
 answ = Answ([10, 15, 3, 7], 17)
 sol = answ.checkArray()
 print(sol)
+
+# This runs in O(n), where it uses dictionary to store value
